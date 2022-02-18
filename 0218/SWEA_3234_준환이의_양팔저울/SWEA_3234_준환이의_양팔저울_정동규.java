5cf7b412e5b006a23ed8b617edc191b61ac0408c
정동규 <1217jdk@naver.com>
2022-02-18 08:59:01 +0000

    2022.02.18 SWEA 3234 준환이의 양팔저울

---
+++ 0218/SWEA_3234_준환이의_양팔저울/SWEA_3234_준환이의_양팔저울_정동규.java
@@ -0,0 +1,92 @@
+package com.ssafy.w0218;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class SWEA_3234_준환이의_양팔저울 {
+     
+    static int res;
+    static int[] factorial = new int[10];
+     
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+         
+        int T = 0, N = 0, sum = 0;
+        boolean[] used;
+        Integer[] weights;
+         
+        initFactorial();
+         
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            sb.append("#").append(t).append(" ");
+             
+            res = 0;
+            sum = 0;
+             
+            N = Integer.parseInt(br.readLine());
+            used = new boolean[N];
+            weights = new Integer[N];
+             
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++) {
+                weights[i] = Integer.parseInt(st.nextToken());
+                sum += weights[i];
+            }
+             
+            weighing(weights, used, 0, 0, sum, 0);
+            sb.append(res).append("\n");
+        }
+        System.out.println(sb);
+    }
+     
+    private static void weighing(Integer[] weights, boolean[] used, int left, int right, int sum, int count) {
+         
+        if(count == weights.length) {
+            res++;
+            return;
+        }
+
+        if(left-right>=sum) {
+            int temp = 1;
+            for(int i=1;i<=weights.length-count;i++)
+               temp*=2;
+            for(int i=2;i<=weights.length-count;i++)
+               temp*=i;
+            res += temp;
+            return;
+         }
+        
+        /*
+        if(left - right >= sum) {
+            res += factorial[weights.length - count] * Math.pow(2, weights.length - count);
+            return;
+        }
+        */
+        
+        for(int i = 0; i < weights.length; i++) {
+            if(used[i] == false) {
+                used[i] = true;
+                weighing(weights, used, left + weights[i], right, sum - weights[i], count + 1);
+                 
+                if(right + weights[i] <= left)
+                    weighing(weights, used, left, right + weights[i], sum - weights[i], count + 1);
+                used[i] = false;
+            }
+        }
+    }
+     
+    private static void  initFactorial() {
+        int num = 1;
+         
+        factorial[0] = 1;
+        for(int i = 1; i <= 9; i++) {
+            num *= i;
+            factorial[i] = num;
+        }
+    }
+}
